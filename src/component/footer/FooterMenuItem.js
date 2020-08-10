ed3cb47758045db7f5f436b6241d99e55124ad19
Your Name <bogdan.panin@gmail.com>
2020-08-10 19:49:46 +0300

    footer menu

--- src/component/footer/FooterMenuItem.js
+++ src/component/footer/FooterMenuItem.js
@@ -6,43 +6,28 @@ import twitter from "../../img/twiter.png";
 import instagram from "../../img/instagram.png";
 
 function FooterMenuItem(props, src) {
-  const itemImg = [
-    {
-      id: 1,
-      Href: "https://www.pinterest.com/"
-    },
-    {
-      id: 2,
-      Href: "https://www.facebook.com/"
-    },
-    {
-      id: 3,
-      Href: "https://twitter.com/"
-    },
-    {
-      id: 4,
-      Href: "https://www.instagram.com/"
-    }
-  ];
   return (
     <div>
       <li className="footer__menu_li">
-        <a href={itemImg.Href} className="footer__menu_a">
+        <a href="https://www.pinterest.com/" className="footer__menu_a">
           <img src={pinterest} alt="" className="footer__menu_img"></img>
         </a>
       </li>
       <li className="footer__menu_li">
-        <a href={itemImg.Href} className="footer__menu_a">
+        <a href="https://www.facebook.com/" className="footer__menu_a">
           <img src={facebook} alt="" className="footer__menu_img"></img>
         </a>
       </li>
       <li className="footer__menu_li">
-        <a href={itemImg.Href} className="footer__menu_a">
+        <a href="https://twitter.com/" className="footer__menu_a">
           <img src={twitter} alt="" className="footer__menu_img"></img>
         </a>
       </li>
       <li className="footer__menu_li">
-        <a href={twitter} className="footer__menu_a">
+        <a
+          href="https://www.instagram.com/ateist_01/"
+          className="footer__menu_a"
+        >
           <img src={instagram} alt="" className="footer__menu_img"></img>
         </a>
       </li>
