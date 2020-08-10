cd4736d70056e223d898fa1e1dbca3ef6d03f0f6
Your Name <bogdan.panin@gmail.com>
2020-08-10 12:25:08 +0300

    Updates

---
+++ precache-manifest.a2db9c0383a897c04a09b0c5871f2058.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d628d2ed52a6190a9db11e26525278",
+    "url": "/react-lending-page/index.html"
+  },
+  {
+    "revision": "a82c068a456e82bb21e0",
+    "url": "/react-lending-page/static/css/main.a6f80948.chunk.css"
+  },
+  {
+    "revision": "285bce63d7bcec7b993a",
+    "url": "/react-lending-page/static/js/2.a57ebb4b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-lending-page/static/js/2.a57ebb4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82c068a456e82bb21e0",
+    "url": "/react-lending-page/static/js/main.09d46a60.chunk.js"
+  },
+  {
+    "revision": "bf02b88b3746053fc964",
+    "url": "/react-lending-page/static/js/runtime-main.26a41e0f.js"
+  },
+  {
+    "revision": "6357a9f89022580c7209e79f2762a65f",
+    "url": "/react-lending-page/static/media/PORTFOLIO3.6357a9f8.png"
+  },
+  {
+    "revision": "cf0a1fca0c7bb69761cb0bce0227c801",
+    "url": "/react-lending-page/static/media/about.cf0a1fca.png"
+  },
+  {
+    "revision": "abfb5c2810cddb299133518d3e2707dd",
+    "url": "/react-lending-page/static/media/book1.abfb5c28.png"
+  },
+  {
+    "revision": "436a05cce5ba208c7c8de5a4f45f5407",
+    "url": "/react-lending-page/static/media/book2.436a05cc.png"
+  },
+  {
+    "revision": "eb158ad395ac949f2b9c0e56204d355d",
+    "url": "/react-lending-page/static/media/book3.eb158ad3.png"
+  },
+  {
+    "revision": "f26d8b9229a62ec669d4c4e9c23f1a5d",
+    "url": "/react-lending-page/static/media/contact.f26d8b92.png"
+  },
+  {
+    "revision": "1964a6438cae319b7d0097abe54594f5",
+    "url": "/react-lending-page/static/media/home.1964a643.png"
+  },
+  {
+    "revision": "723955ea97da93374b6b69d1537a4b3d",
+    "url": "/react-lending-page/static/media/portfolio1.723955ea.png"
+  },
+  {
+    "revision": "d70d1c9ab828d0e3c907890a3fd9e718",
+    "url": "/react-lending-page/static/media/portfolio2.d70d1c9a.png"
+  }
+]);
